61070d044d7be9ae08f702bc85c27a73a2258c69
Jasmine <jaasmine2001@gmail.com>
2023-10-26 20:42:57 +0530

    completed

--- app.js
+++ app.js
@@ -1,32 +1,42 @@
-// const input = document.getElementById("input");
-// const list = document.getElementById("list");
-
-// function addTask() {
-//     if (input.value === '') {
-//         alert("You must write something!");
-//     } else {
-//         let li = document.createElement("li");
-//         li.innerHTML = input.value;
-//         list.appendChild(li);
-//     }
-//     input.value = ""; // Clear the input field by setting it to an empty string
-// }
-
-
 const input = document.getElementById("input");
 const list = document.getElementById("list");
 
 function addTask() {
-    console.log("addTask function called");
-    console.log("Input value: " + input.value);
-
     if (input.value === '') {
         alert("You must write something!");
     } else {
         let li = document.createElement("li");
         li.innerHTML = input.value;
         list.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+
     }
-    input.value = "";
-    console.log("Input value after clearing: " + input.value);
+    input.value = ""; // Clear the input field by setting it to an empty string
+    saveData();//function to store data in browser
 }
+
+list.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+        
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+//to store in browser
+
+function saveData(){
+    localStorage.setItem("data", list.innerHTML);
+}
+
+function showTask(){
+    list.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
